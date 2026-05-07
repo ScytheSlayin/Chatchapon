bc7c438d7a4893782e5bc20564f9b513a64bce27
ScytheSlayin <??>
2026-05-06 22:42:02 -0700

    x11 testing

--- Menu.java
+++ Menu.java
@@ -1,1 +1,21 @@
-// WIP3
+import javax.swing.*;
+import java.awt.*;
+
+// https://docs.oracle.com/javase/8/docs/api/javax/swing/JFrame.html
+// https://www.geeksforgeeks.org/java/java-jframe/
+
+
+public class Menu{
+
+	public static void main(String[] args){
+
+	// create display window
+	// Scanner scan = new scanner logic
+	JFrame screen = new JFrame("Cuphead 41");
+
+	// creates panel
+	JPanel panel = new JPanel();
+
+	panel.setBackground(Color.BLACK);
+	}
+}
