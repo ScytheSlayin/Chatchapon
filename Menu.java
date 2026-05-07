c5a65898485ceebad0546bc4692c9d73ce1acee3
ScytheSlayin <??>
2026-05-06 23:06:41 -0700

    Menu with terminal, need to learn x11 to make smooth movement

--- Menu.java
+++ Menu.java
@@ -1,21 +1,62 @@
-import javax.swing.*;
-import java.awt.*;
+// Use if implementing sprites and X11 fowarding with XLaunc, terminal logic for now cause this is confusing
+
+// import javax.swing.*;
+// import java.awt.*;
 
 // https://docs.oracle.com/javase/8/docs/api/javax/swing/JFrame.html
 // https://www.geeksforgeeks.org/java/java-jframe/
 
+import java.util.Scanner;
 
 public class Menu{
 
 	public static void main(String[] args){
 
-	// create display window
-	// Scanner scan = new scanner logic
-	JFrame screen = new JFrame("Cuphead 41");
+		Scanner input = new Scanner(System.in);
 
-	// creates panel
-	JPanel panel = new JPanel();
+		String[] options = {"FIGHT", "SHOP", "CONTROLS", "QUIT"};
 
-	panel.setBackground(Color.BLACK);
+		int selected = 0;
+
+		// use for frame switching later
+		boolean isRunning = true;
+
+		while(isRunning){
+
+
+			System.out.flush();
+
+			System.out.println("========================================");
+			System.out.println("|    Cuphead 41    |");
+			//System.out.println("========================================");
+
+			// make arrow on user selection
+
+			for(int i = 0; i < options.length; i++){
+				if(i == selected){
+					System.out.println("|            > [ " + options[i] + " ]             |");
+				}
+				else{
+					System.out.println("|              [ " + options[i] + " ]             |");
+				}
+			}
+
+
+			// bottom half
+
+			System.out.println("|                                      |");
+			System.out.println("========================================");
+			System.out.println();
+			System.out.println("     W/S for Up & Down | E to Select    ");
+			
+			String choice = input.nextLine();
+
+			
+
+
+
+
+		}
+
 	}
 }
