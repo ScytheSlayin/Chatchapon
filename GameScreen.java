8bc5ddfa346d4abec6a3c97c2772c8e20599c662
ScytheSlayin <??>
2026-05-15 01:49:58 -0700

    added inhertiance from powerups, now applyins, movement control set

--- GameScreen.java
+++ GameScreen.java
@@ -33,3 +33,119 @@ public class GameScren extends JPanel implements KeyListener, ActionListener{
 	boolean down;
 	boolean left;
 	boolean right;
+
+	int pFrame = 0;
+
+	// boss stuff
+
+	int bossX = 620;
+	int bossY = 220;
+
+	int bossHealth = 300;
+	int bossDir = 1;
+	int bFrame = 0;
+
+	// powerups if applicable
+
+	String power = "None";
+
+	boolean hasSecond = false;
+	boolean hasSheild = false;
+
+
+	// bullet sprites
+	ArrayList<Bullet> enemyBullets = new ArrayList<Bullet>();
+	ArrayList<Bullet> playerBullets = new ArrayList<Bullet>();
+
+	Random rand = new Random();
+
+	Timer timer;
+
+	int frameCount = 0;
+
+	boolean gameOver = false;
+	boolean playerWon = false;
+
+	public GameScreen(String powerupFromMenu) {
+
+		powerup = powerupFromMenu;
+
+		applyPowerup();
+
+		setPreferredSize(new Dimension(900, 650));
+		setFocusable(true);
+		addKeyListener(this);
+
+		timer = new Timer(16, this); // like 60 FPS
+		timer.start();
+	}
+ void applyPowerup() {
+
+	if (powerup.contains("Faster Speed")) {
+		playerSpeed = 8;
+	}
+
+	if (powerup.contains("More Health")) {
+		playerHealth = 150;
+	}
+
+	if (powerup.contains("Sheild")) {
+		shield = true;
+	}
+
+	if (powerup.contains("Second Life")) {
+		secondLife = true;
+	}
+}
+
+public void actionPerformed(ActionEvent e) {
+
+	if (gameOver == false) {
+
+		updatePlayer();
+		updateBoss();
+		spawnEnemyBullets();
+		updateBullets();
+		isColliding();
+		updateAnimation();
+	}
+
+	repaint();
+}
+
+public void updatePlayer() {
+	// test this debug 
+	if (up) {
+		playerY -= playerSpeed;
+	}
+
+	if (down) {
+		playerY += playerSpeed;
+	}
+
+	if (left) {
+		playerX -= playerSpeed;
+	}
+
+	if (right) {
+		playerX += playerSpeed;
+	}
+// bound
+	if (playerX < 0) {
+	playerX = 0;
+	}
+
+if (playerX > 840) {
+	playerX = 840;
+}
+
+if (playerY < 0) {
+	playerY = 0;
+}
+
+if (playerY > 570) {
+	playerY = 570;
+}
+}
+
+
